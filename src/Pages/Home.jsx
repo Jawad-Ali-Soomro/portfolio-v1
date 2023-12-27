77ab5d0317f3ee12eff12c9bdb668fbf2c050bd1
Mr Hacker 🦁 <jawadal829@gmail.com>
2023-12-27 15:17:37 +0500

    upadted site

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -3,7 +3,13 @@ import Typed from "typed.js";
 import { useEffect } from "react";
 
 const Home = () => {
+  const [click, handleClick] = useState(false);
   const [dark, setDark] = useState(false);
+  const [tab, setTab] = useState("/");
+  const setNavigate = (e) => {
+    e.preventDefault();
+    setTab(e.target.value);
+  };
   const el = React.useRef(null);
   useEffect(() => {
     const typed = new Typed(el.current, {
@@ -25,23 +31,15 @@ const Home = () => {
   return (
     <div>
       <div className="header">
-        <img
-          src={`${dark == true ? "./logo-black.jpg" : "./logo-white.jpg"}`}
-          alt=""
-        />
-        <div className="main-menu">
-          <div
-            className="bar"
-            style={{ background: `${dark == true ? "white" : "black"}` }}
-          ></div>
-          <div
-            className="bar"
-            style={{ background: `${dark == true ? "white" : "black"}` }}
-          ></div>
-          <div
-            className="bar"
-            style={{ background: `${dark == true ? "white" : "black"}` }}
-          ></div>
+        <div className="logo">
+          <h1>
+            <h2>WEB</h2> <span>TECH</span>
+          </h1>
+        </div>
+        <div className="main-menu" onClick={() => handleClick(true)}>
+          <div className="bar" style={{ background: "orange" }}></div>
+          <div className="bar" style={{ background: "orange" }}></div>
+          <div className="bar" style={{ background: "orange" }}></div>
         </div>
       </div>
       <div
@@ -57,12 +55,7 @@ const Home = () => {
         <h1 style={{ color: `${dark == true ? "white" : "black"}` }}>
           To The Site Of
         </h1>
-        <h2
-          // style={{
-          //   WebkitTextStroke: `${dark == true ? "2px white" : "2px black"}`,
-          // }}
-          ref={el}
-        ></h2>
+        <h2 ref={el}></h2>
         <i
           style={{ color: `${dark == true ? "white" : "black"}` }}
           className="uil uil-arrow-down arrow-down"
@@ -89,6 +82,86 @@ const Home = () => {
         >
           <i className="uil uil-sun"></i>
         </div>
+      </div>
+      <div
+        className={click == true ? "mobile-menu-on" : "mobile-menu"}
+        style={{ background: `${dark == true ? "black" : "white"}` }}
+      >
+        <i
+          className="uil uil-multiply close-btn"
+          style={{ color: "orange" }}
+          onClick={() => handleClick(false)}
+        ></i>
+        {tab == "/" ? (
+          <a href="" className="active">
+            Home
+          </a>
+        ) : (
+          <a
+            href=""
+            className="not-active"
+            style={{ color: `${dark == true ? "white" : "black"}` }}
+            onClick={(e) => e.preventDefault() + setTab("/")}
+          >
+            Home
+          </a>
+        )}
+        {tab == "/About" ? (
+          <a href="" className="active">
+            About
+          </a>
+        ) : (
+          <a
+            href=""
+            className="not-active"
+            style={{ color: `${dark == true ? "white" : "black"}` }}
+            onClick={(e) => e.preventDefault() + setTab("/About")}
+          >
+            About
+          </a>
+        )}
+        {tab == "/Team" ? (
+          <a href="" className="active">
+            Team
+          </a>
+        ) : (
+          <a
+            href=""
+            className="not-active"
+            style={{ color: `${dark == true ? "white" : "black"}` }}
+            onClick={(e) => e.preventDefault() + setTab("/Team")}
+          >
+            Team
+          </a>
+        )}
+        {tab == "/Work" ? (
+          <a href="" className="active">
+            Work
+          </a>
+        ) : (
+          <a
+            href=""
+            className="not-active"
+            style={{ color: `${dark == true ? "white" : "black"}` }}
+            onClick={(e) => e.preventDefault() + setTab("/Work")}
+          >
+            Work
+          </a>
+        )}
+        {tab == "/Contact" ? (
+          <a href="" className="active">
+            Contact
+          </a>
+        ) : (
+          <a
+            href=""
+            className="not-active"
+            style={{ color: `${dark == true ? "white" : "black"}` }}
+            onClick={(e) => e.preventDefault() + setTab("/Contact")}
+          >
+            Contact
+          </a>
+        )}
       </div>
     </div>
   );
