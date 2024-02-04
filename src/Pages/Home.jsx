644fcc8fa81c484b03fa56f36920e3697e370b90
Mr Hacker 🦁 <jawadal829@gmail.com>
2024-02-04 21:37:51 +0500

    updated

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,25 +1,87 @@
 import React, { useState } from "react";
-import Header from "../Components/Header";
+import {} from "react-icons/bi";
 
 const Home = () => {
+  const [showMenu, steShowMenu] = useState(false);
+  const [tab, setTab] = useState(window.location.pathname);
   return (
-    <div>
-      <Header />
-      <div className="hero-sect flex col">
-        <h1>I'm Web Designer & Developer</h1>
-        <p>
-          Hello! I'm Jawad, a freelance web interface designer & developer based
-          in Pakistan. I’m very passionate about the work that I do.{" "}
-        </p>
-        <div className="btns">
-          <button>My Projects</button>
-          <button>Contact Me</button>
+    <div className="home-page flex col">
+      <div className="top-nav flex between">
+        <div className="logo">
+          <a href="" className="flex">
+            <p>J</p>
+            <span>awad</span>
+          </a>
         </div>
-        <div className="my-email">
-          <a href="">jawadal829@gmail.com</a>
+        <div className="navs">
+          <div className="icon flex col" onClick={() => steShowMenu(true)}>
+            <div className="flex" style={{ gap: "5px" }}>
+              <div className="circle"></div>
+              <div className="circle"></div>
+              <div className="circle"></div>
+            </div>
+            <div className="flex" style={{ gap: "5px" }}>
+              <div className="circle"></div>
+              <div className="circle"></div>
+              <div className="circle"></div>
+            </div>
+            <div className="flex" style={{ gap: "5px" }}>
+              <div className="circle"></div>
+              <div className="circle"></div>
+              <div className="circle"></div>
+            </div>
+          </div>
         </div>
       </div>
-      <div className="about-me"></div>
+      <div className="content">
+        <h1>
+          Hey There! I'm Jawad <br />
+          You Can Call ME MR Jawad
+        </h1>
+        <p>I am Web Developer , Ui/ux designer & full stack developer</p>
+        <div className="btns">
+          <button>See My Projects</button>
+          <button>About ME</button>
+        </div>
+      </div>
+      <div
+        className="main-menu"
+        style={{ maxWidth: `${showMenu == true ? "100%" : "0%"}` }}
+      >
+        <i
+          className="uil uil-multiply close-btn"
+          onClick={() => steShowMenu(false)}
+        ></i>
+        <div className="menu-navs">
+          <ul>
+            {tab == "/" ? <li className="active">Home</li> : <li>not home</li>}
+            <li>About</li>
+            <li>Contact</li>
+            <li>Projects</li>
+          </ul>
+          <div className="links flex">
+            <a href="">jawadal829@gmail.com</a>
+            <a href="">+923173270808</a>
+            <div className="social">
+              <a href="">
+                <i className="uil uil-youtube"></i>
+              </a>
+              <a href="">
+                <i className="uil uil-facebook"></i>
+              </a>
+              <a href="">
+                <i className="uil uil-instagram"></i>
+              </a>
+              <a href="">
+                <i className="uil uil-twitter"></i>
+              </a>
+              <a href="">
+                <i className="uil uil-github-alt"></i>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
